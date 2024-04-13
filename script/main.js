83a3acca5981ace1bcdf936f1cf30024e4db9d80
eldhose-oss <eldhosebabu197@gmail.com>
2024-04-13 06:27:25 -0400

    Allocated price for each weapons using math.random

--- script/main.js
+++ script/main.js
@@ -4,8 +4,9 @@ const WEAPONS_URL = "https://bymykel.github.io/CSGO-API/api/en/skins.json";
 
 //array to store api data
 let agentArray = [];
-let weaponsArray = [];
-
+// let weaponsArray = [];
+let counterTerroristWeapons = [];
+let terroristsWeapons = [];
 //function to fetch api to call api and set data.
 function apiFetch(url, callbackfunction) {
     let xhr = new XMLHttpRequest();
@@ -20,10 +21,79 @@ apiFetch(AGENT_URL, (response) => {
     agentArray = JSON.parse(response);
     console.log(agentArray); //for debugging purpose
 });
+//function to get random number
+function getRandomNumber(min, max) {
+    // Adjust min and max to ensure they're multiples of 50
+    min = Math.ceil(min / 50) * 50;
+    max = Math.floor(max / 50) * 50;
+
+    // Calculate the number of possible values within the range
+    var numPossibleValues = (max - min) / 50 + 1;
+
+    // Generate a random index within the range of possible values
+    var randomIndex = Math.floor(Math.random() * numPossibleValues);
+
+    // Calculate and return the random number
+    return min + randomIndex * 50;
+}
 //call to do api fetch 
 apiFetch(WEAPONS_URL, (response) => {
-    weaponsArray = JSON.parse(response);
-    console.log(weaponsArray); //for debugging purpose
+    let min;
+    let max;
+    let tempArray = JSON.parse(response);
+    for (let i = 0; i < tempArray.length; i++) {
+        if (tempArray[i].category.name !== null)
+            if (tempArray[i].image !== null)
+                if (tempArray[i].pattern !== null)
+                    if (tempArray[i].team.id !== null)
+                        if (tempArray[i].weapon.name !== null) {
+                            if(tempArray[i].category.name === 'Pistols'){
+                                min = 200;
+                                max = 700;
+                                tempArray[i].price = getRandomNumber(min, max);
+                            }
+                            if(tempArray[i].category.name === 'Rifles'){
+                                min = 1500;
+                                max = 3500;
+                                tempArray[i].price = getRandomNumber(min,max);
+                            }
+                            if(tempArray[i].category.name === 'Heavy'){
+                                min = 2500;
+                                max = 4500;
+                                tempArray[i].price = getRandomNumber(min,max);
+                            }
+                            if(tempArray[i].category.name === 'SMGs'){
+                                min = 1000;
+                                max = 1500;
+                                tempArray[i].price = getRandomNumber(min,max);
+                            }
+                            if(tempArray[i].category.name === 'Knives'){
+                                min = 100;
+                                max = 500;
+                                tempArray[i].price = getRandomNumber(min,max);
+                            }
+                            if(tempArray[i].category.name === 'Gloves'){
+                                min = 100;
+                                max = 500;
+                                tempArray[i].price = getRandomNumber(min,max);
+                            }
+                            if (tempArray[i].team.id === 'both') {
+                                counterTerroristWeapons.push(tempArray[i]);
+                                terroristsWeapons.push(tempArray[i]);
+                            }
+                            else if (tempArray[i].team.id === 'counter-terrorists') {
+                                counterTerroristWeapons.push(tempArray[i]);
+                            } else if (tempArray[i].team.id === 'terrorists') {
+                                terroristsWeapons.push(tempArray[i]);
+                            }
+
+                        } else {
+                            continue;
+                        }
+    }
+    tempArray = [];
+    console.log(terroristsWeapons);
+    console.log(counterTerroristWeapons);
 });
 //function to call selected team
 function selectedTeam(teamName) {
@@ -98,13 +168,13 @@ function agentSelect(classname) {
     }
 }
 //funtion to display error message
-function errorDisplay(message){
+function errorDisplay(message) {
     let textSpan = document.getElementById("errorMessage");
     textSpan.innerText = message;
     textSpan.style.padding = ".5em";
 }
 //to go to weapon section 
-function goToWeaponSelection(){
+function goToWeaponSelection() {
     console.log("here");
     let inputBox = document.getElementById("input-name");
     let value = inputBox.value.trim();
@@ -113,35 +183,43 @@ function goToWeaponSelection(){
     let flagAgent = false;
     let flagName = false;
     console.log(isActive)
-    if(isActive == false){
+    if (isActive == false) {
         flagAgent = false;
         console.log(isActive + "in")
         errorDisplay("Choose your agent!");
         return;
-    } else { 
+    } else {
         flagAgent = true;
     }
-    
-    if(value === ""){
+
+    if (value === "") {
         flagName = false;
         errorDisplay("Username can't be empty!");
-    }else if(characterCount > 20){
+    } else if (characterCount > 20) {
         flagName = false;
         errorDisplay("Invalid character length. No more than 20!");
-    }else if(wordCount > 2){
+    } else if (wordCount > 2) {
         flagName = false;
         errorDisplay("Invalid word count. No more than 2 words!");
     }
-    else{
+    else {
         flagName = true;
         errorDisplay("");
         localStorage.setItem("username", value);
     }
-    if(flagAgent === true && flagName === true){
-
-        window.location.href = "";
-    }   
+    if (flagAgent === true && flagName === true) {
+        window.location.href = "weapon-select.html";
+    }
 }
+//to sort and display weapons
+//heavy,rifle,smg,pistols,knife,gloves
+//category.name, 
+
+//display weapons to respective category
+function showWeaponsToCategory() {
+
+}
+
 
 
 
